10a38c52fb4f8b711a88e99f5bf51ffdc40d9cfe
WUSO01 <Lovemyself55>
2017-05-03 16:58:41 +0800

    注册功能雏形

--- routes/user/user.js
+++ routes/user/user.js
@@ -1,11 +1,22 @@
 const User = require('../../models/user');
 
-
 /* register page. */
 exports.showRegister = (req, res) => {
   res.render('pages/register');
 }
 
 exports.register = (req, res) => {
+  let _req = req.body;
+  // 获取用户名 密码 邮箱
+  let [name, password, re_password, email] = [_req.name, _req.password, _req.re_password, _req.email];
+  User.create({
+    name: name,
+    password: password,
+    email: email
+  }, err => {
+    if (err) next (err);
+    res.redirect('/');
+  })
+  
 }
 
