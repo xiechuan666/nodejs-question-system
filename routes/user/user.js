a0d4de1f9b725c1ac0a2822c659acaa9be419f02
WUSO01 <Lovemyself55>
2017-05-03 17:15:51 +0800

    密码加密

--- routes/user/user.js
+++ routes/user/user.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const User = require('../../models/user');
 
 /* register page. */
@@ -9,6 +10,13 @@ exports.register = (req, res) => {
   let _req = req.body;
   // 获取用户名 密码 邮箱
   let [name, password, re_password, email] = [_req.name, _req.password, _req.re_password, _req.email];
+  
+  /* 创建一个md5的hash实例 */
+  let md5 = crypto.createHash('md5');
+  md5.update(password);
+  password = md5.digest('hex')  // 把密码转换成16进制的字符串
+
+  /* 存入数据库 */
   User.create({
     name: name,
     password: password,
@@ -17,6 +25,6 @@ exports.register = (req, res) => {
     if (err) next (err);
     res.redirect('/');
   })
-  
+
 }
 
