037657eedc31c9127f9b0af4d8798427654c66bd
WUSO01 <Lovemyself55>
2017-05-04 17:14:56 +0800

    图片上传

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -1,1 +1,31 @@
-$('#textEdit').trumbowyg();
+// $('#textEdit').trumbowyg({
+//   btnsDef: {  
+//     image: {  
+//       dropdown: [ 'insertImage', 'base64','upload' ],  // 'upload' 还有上传图片  
+//       ico: 'insertImage'  
+//     }  
+//   },  
+// });
+$.trumbowyg.btnsGrps.test = ['bold', 'link'];
+$('#textEdit').trumbowyg({
+  lang: 'zh_cn',
+  closable: true,
+  fixedBtnPane: true,
+  btnsDef: {
+    // Customizables dropdowns
+    align: {
+      dropdown: ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+      ico: 'justifyLeft'
+    },
+    image: {
+      dropdown: ['insertImage', 'upload', 'base64'],
+      ico: 'insertImage'
+    }
+  },
+  btns: ['viewHTML',
+    '|', 'formatting',
+    '|', 'btnGrp-test',
+    '|', 'align',
+    // '|', 'btnGrp-lists',
+    '|', 'image']
+});
