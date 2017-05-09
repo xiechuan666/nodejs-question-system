52e5cd16b8a87bb0a4dbe4fbe0705c05a3147d98
WUSO01 <Lovemyself55>
2017-05-09 11:12:34 +0800

    clear

--- config/mongoose.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/zy1390');
-
-exports.mongoose = mongoose
--- models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('../config/mongoose').mongoose;
-
-var schema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String
-});
-
-module.exports = mongoose.model('User', schema);
